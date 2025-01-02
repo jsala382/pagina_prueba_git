ebc0e954ef10caa700a9d4500eb3b87484e5bbb3
jsala382 <naruto_011990@hotmail.com>
2025-01-02 16:13:05 -0500

    Cambio de titulo de la pagina
    Cambio el titulo de la pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,26 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  mensaje: string="Estamos en el segundo dia del 2025"
+  constructor(private titleService: Title) {
+    this.titleService.setTitle("Prueba de git");
+  }
+  
+  cambiarMensaje() {
+    this.mensaje = '¡Gracias por hacer clic!';
+  }
+}
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  titulo : string = "Pagina de prueba en git"
   mensaje: string="Estamos en el segundo dia del 2025"
   cambiarMensaje() {
     this.mensaje = '¡Gracias por hacer clic!';
